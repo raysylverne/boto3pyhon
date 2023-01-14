b7a2ede5439607a29442bac0df9ed459e162f1bf
Ray Sylverne <raysylverne@gmail.com>
2023-01-14 11:16:46 -0500

    Append, insert, delete, and  slice from a list.

--- fundemental.py
+++ fundemental.py
@@ -16,3 +16,42 @@ print(x[2:13:2])
 
 # -1 by passing a negative number the output will be produced in reversed
 print(x[::-1])
+
+
+# LIST
+# index 0  1  2  3   4   5
+data = [1, 2, 3, 4, 'a', 10.4, 2]
+
+
+for x in data:
+    print(x)
+
+# use append() to add object at the end of a list
+data.append(34)
+print(data)
+
+# use insert() to add and object to any location within a list
+# by declaring the index location within the function
+data.insert(3, 'Python')
+print(data)
+
+# use del [] to delete an object from any location within a list
+# by declaring the index location within the function
+del data[3]
+print(data)
+
+# slicing a list
+
+data = [1, 2, 3, 4, 'a', 10.4, 2]
+print(data[2::2])
+# output will be [3, 'a', 2]
+
+
+
+
+
+
+
+
+
+
